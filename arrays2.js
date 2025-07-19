e9e95b3d0aeafd087a57fd5daae75c71b4c30cf3
lukefrederick <lfrederick220@gmail.com>
2025-07-18 19:47:36 -0500

    Submission

---
+++ arrays2.js
@@ -0,0 +1,18 @@
+// Task one
+let arrayOne = Array(7);
+arrayOne.fill("Hello");
+console.log(arrayOne);
+
+// Task 2
+// Emptying out array
+let arrayTwo = Array(7);
+arrayTwo.fill("Hi" , 2, 5);
+console.log(arrayTwo);
+
+// Task 3
+let numbers = Array(5);
+for (let i = 0; i  <= 4; i++) {
+    numbers[i] = i * 10;
+}
+console.log(numbers);
+
